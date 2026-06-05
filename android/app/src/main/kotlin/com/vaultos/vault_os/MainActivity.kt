ac9d9d53bb80c8565bb1adf014733968d140a1c7
Nevy11 <smongare2004@gmail.com>
2026-06-05 15:59:48 +0300

    modify build mode

--- android/app/src/main/kotlin/com/vaultos/vault_os/MainActivity.kt
+++ android/app/src/main/kotlin/com/vaultos/vault_os/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.vaultos.vault_os
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
